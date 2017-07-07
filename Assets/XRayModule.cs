eb8a97b2d2307134e473c3605374a5bc108fa235
Timwi <github@timwi.de>
2017-07-08 00:48:19 +0200

    Initial commit

---
+++ Assets/XRayModule.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using XRay;
+using UnityEngine;
+using Rnd = UnityEngine.Random;
+
+/// <summary>
+/// On the Subject of X-Rays
+/// Created by Timwi
+/// </summary>
+public class XRayModule : MonoBehaviour
+{
+    public KMBombInfo Bomb;
+    public KMBombModule Module;
+    public KMAudio Audio;
+
+    private static int _moduleIdCounter = 1;
+    private int _moduleId;
+
+    void Start()
+    {
+        _moduleId = _moduleIdCounter++;
+    }
+}
