88882d27729d029f7e67f89f80e33d679579d323
Timwi <github@timwi.de>
2017-07-14 05:14:47 +0200

    Scanner

--- Assets/XRayModule.cs
+++ Assets/XRayModule.cs
@@ -1,8 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections;
 using System.Linq;
-using XRay;
 using UnityEngine;
+using XRay;
+
 using Rnd = UnityEngine.Random;
 
 /// <summary>
@@ -15,11 +15,98 @@ public class XRayModule : MonoBehaviour
     public KMBombModule Module;
     public KMAudio Audio;
 
+    public GameObject[] ScanLights;
+
+    private const int _iconWidth = 180;
+    private const int _iconHeight = 180;
+
+    private static int[] _table = { 30, 28, 26, 1, 25, 11, 8, 29, 9, 12, 3, 11, 15, 13, 28, 19, 14, 31, 0, 15, 13, 31, 18, 9, 8, 11, 2, 9, 23, 22, 17, 14, 0, 23, 32, 20, 30, 27, 25, 4, 26, 16, 9, 24, 23, 6, 4, 28, 12, 15, 21, 10, 21, 22, 29, 17, 26, 27, 22, 32, 0, 7, 15, 17, 14, 23, 2, 11, 27, 27, 23, 18, 13, 25, 9, 11, 19, 19, 4, 14, 16, 32, 5, 12, 3, 10, 0, 5, 32, 25, 30, 28, 3, 19, 6, 22, 18, 10, 24, 20, 6, 13, 16, 16, 7, 5, 30, 18, 29, 31, 1, 31, 21, 3, 1, 17, 20, 20, 5, 32, 17, 2, 1, 29, 7, 15, 16, 19, 24, 4, 7, 22, 26, 5, 4, 27, 6, 12, 14, 6, 10, 31, 18, 21 };
+
     private static int _moduleIdCounter = 1;
     private int _moduleId;
 
     void Start()
     {
         _moduleId = _moduleIdCounter++;
+
+        var col = Rnd.Range(0, 12);
+        var row = Rnd.Range(0, 12);
+        // This makes sure that we don’t go off the edge of the table
+        var dir = Enumerable.Range(0, 9).Where(dr => !(col == 0 && dr % 3 == 0) && !(col == 11 && dr % 3 == 2) && !(row == 0 && dr / 3 == 0) && !(row == 11 && dr / 3 == 2)).PickRandom();
+        var solution = _table[(row + dir / 3 - 1) * 12 + col + (dir % 3 - 1)];
+        var buttons = Enumerable.Range(0, 33).Where(i => i != solution).ToList().Shuffle().Take(4).Concat(new[] { solution }).ToList().Shuffle();
+
+        Debug.LogFormat("[X-Ray #{0}] Column {1}, Row {2}: symbol there is {3}", _moduleId, col + 1, row + 1, _table[row * 12 + col]);
+        Debug.LogFormat("[X-Ray #{0}] {1}: symbol is now {2}", _moduleId, "move up-left,move up,move up-right,move left,stay put,move right,move down-left,move down,move down-right".Split(',')[dir], solution);
+
+        StartCoroutine(RunLights(col, row, dir));
+    }
+
+    private IEnumerator RunLights(int col, int row, int dir)
+    {
+        var icons = new[] { col, row + 12, dir + 24 };
+        icons.Shuffle();
+
+        const int _ulongsPerScanline = (_iconWidth + 63) / 64;
+        const float _pixelWidth = 0.138f / _iconWidth;
+
+        var curScanline = 0;
+        var curDirectionNeg = false;
+
+        var mode = Rnd.Range(0, 3);  // 0 = top to bottom; 1 = bottom to top; 2 = back and forth
+        Debug.LogFormat("[X-Ray #{0}] Scanning {1}.", _moduleId, "from top to bottom,from bottom to top,back and forth".Split(',')[mode]);
+        if (mode == 1)
+        {
+            curScanline = 3 * _iconHeight - 1;
+            curDirectionNeg = true;
+        }
+
+        while (true)
+        {
+            var icon = RawBits.Icons[icons[curScanline / _iconHeight]];
+            var scanlineStart = (curScanline % _iconHeight) * _ulongsPerScanline;
+            var lightIx = 0;
+            int? startX = null;
+            for (int x = 0; x <= _iconWidth; x++)
+            {
+                var curBit = x < _iconWidth && (icon[scanlineStart + x / 64] & (1UL << (x % 64))) != 0;
+                if (curBit && startX == null)
+                    startX = x;
+                else if (!curBit && startX != null)
+                {
+                    ScanLights[lightIx].SetActive(true);
+                    ScanLights[lightIx].transform.localScale = new Vector3((x - startX.Value) * _pixelWidth, 1, 1);
+                    ScanLights[lightIx].transform.localPosition = new Vector3((x + startX.Value - _iconWidth) * .5f * _pixelWidth, 0, 0);
+                    lightIx++;
+                    startX = null;
+                }
+            }
+            for (int i = lightIx; i < ScanLights.Length; i++)
+                ScanLights[i].SetActive(false);
+
+            var elapsed = 0f;
+            while (elapsed < 1f / 60f)
+            {
+                yield return null;
+                elapsed += Time.deltaTime;
+            }
+
+            if (curDirectionNeg)
+            {
+                curScanline--;
+                if (mode == 1 && curScanline == -1)
+                    curScanline = 3 * _iconHeight - 1;
+                else if (mode == 2 && curScanline == 0)
+                    curDirectionNeg = false;
+            }
+            else
+            {
+                curScanline++;
+                if (mode == 0 && curScanline == 3 * _iconHeight)
+                    curScanline = 0;
+                else if (mode == 2 && curScanline == 3 * _iconHeight - 1)
+                    curDirectionNeg = true;
+            }
+        }
     }
 }
