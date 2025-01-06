ff836aaeb6292def92f5fee72baaf81835945bbc
Timwi <github@timwi.de>
2025-01-06 13:06:52 +0100

    * X-Ray: expose column and row for Souvenir * Modkit updates

--- Assets/Scripts/XRayModule.cs
+++ Assets/Scripts/XRayModule.cs
@@ -20,6 +20,8 @@ public class XRayModule : XRayModuleBase
     private int _moduleId;
     private int _solution;
     private XRayRules _rules;
+    private int _col;  // for Souvenir
+    private int _row;  // for Souvenir
 
     static SymbolInfo convertForSeed1(int icon)
     {
@@ -95,17 +97,17 @@ public class XRayModule : XRayModuleBase
 
     private void Initialize()
     {
-        var col = Rnd.Range(0, 12);
-        var row = Rnd.Range(0, 12);
+        _col = Rnd.Range(0, 12);
+        _row = Rnd.Range(0, 12);
 
         // This makes sure that we don’t go off the edge of the table
-        var dir = Enumerable.Range(0, 9).Where(dr => !(col == 0 && dr % 3 == 0) && !(col == 11 && dr % 3 == 2) && !(row == 0 && dr / 3 == 0) && !(row == 11 && dr / 3 == 2)).PickRandom();
-        _solution = _rules.NumbersInTable[(row + dir / 3 - 1) * 12 + col + (dir % 3 - 1)];
+        var dir = Enumerable.Range(0, 9).Where(dr => !(_col == 0 && dr % 3 == 0) && !(_col == 11 && dr % 3 == 2) && !(_row == 0 && dr / 3 == 0) && !(_row == 11 && dr / 3 == 2)).PickRandom();
+        _solution = _rules.NumbersInTable[(_row + dir / 3 - 1) * 12 + _col + (dir % 3 - 1)];
 
-        Debug.LogFormat("[X-Ray #{0}] Column {1}, Row {2}: number there is {3}.", _moduleId, _rules.Columns[col], _rules.Rows[row], _rules.NumbersInTable[row * 12 + col] + 1);
+        Debug.LogFormat("[X-Ray #{0}] Column {1}, Row {2}: number there is {3}.", _moduleId, _rules.Columns[_col], _rules.Rows[_row], _rules.NumbersInTable[_row * 12 + _col] + 1);
         Debug.LogFormat("[X-Ray #{0}] {1} = {2}. Solution is {3}.", _moduleId, _rules.Table3x3[dir], "Move up-left,Move up,Move up-right,Move left,Stay put,Move right,Move down-left,Move down,Move down-right".Split(',')[dir], _solution + 1);
 
-        var icons = new[] { _rules.Columns[col], _rules.Rows[row], _rules.Table3x3[dir] };
+        var icons = new[] { _rules.Columns[_col], _rules.Rows[_row], _rules.Table3x3[dir] };
         icons.Shuffle();
         var mode = (ScanningMode) Rnd.Range(0, 3);
         Debug.LogFormat("[X-Ray #{0}] Scanning {1}.", _moduleId,
